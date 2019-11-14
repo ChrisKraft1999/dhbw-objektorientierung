9a8883f6cc0965d7cd70627af4516baebd7eb800
schmitt.simon2 <schmitt.simon2@348A07>
2019-11-14 15:58:37 +0100

    Figur überarbeitung

--- Beispielprojekt/Spiel.cpp
+++ Beispielprojekt/Spiel.cpp
@@ -12,14 +12,26 @@
 // Simulationsgeschwindigkeit
 const double DT = 100.0;
 
+const int x_groesse_rahmen = 600;
+const int y_groesse_rahmen = 800;
+
 class Figur {
 public:
 	int x_pos;
 	int y_pos;
+	int x_laenge;
+	int y_laenge;
 	int leben;
 	Gosu::Image bild;
 	void positioniere(int x, int y) {
-		bild.draw(x, y, 0.0);
+		this-> x_laenge = bild.width();
+		this-> y_laenge = bild.height();
+		if (x > (x_groesse_rahmen - x_laenge)) {
+			x = x_groesse_rahmen - x_laenge;
+		}
+		if (y > (y_groesse_rahmen - y_laenge)) {
+			y = y_groesse_rahmen - y_laenge;
+		}
 		this-> x_pos = x;
 		this-> y_pos = y;
 	}
@@ -32,7 +44,7 @@ class GameWindow : public Gosu::Window
 {
 public:
 	GameWindow()
-		: Window(800, 600), Gegner1("GegnerTyp1.png")
+		: Window(x_groesse_rahmen, y_groesse_rahmen), Gegner1("GegnerTyp1.png")
 	{
 		set_caption("Github Test"); // Name lautet nicht Qwertz
 	}
@@ -42,12 +54,12 @@ public:
 	// Wenn die Grafikkarte oder der Prozessor nicht mehr hinterherkommen,
 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
 	void draw() override{
-		Gegner1.positioniere(100,100);
+		Gegner1.bild.draw(Gegner1.x_pos, Gegner1.y_pos, 0.0);
 	}
 
 	// Wird 60x pro Sekunde aufgerufen
-	void update() override
-	{
+	void update() override{
+		Gegner1.positioniere(100, 100);
 	}
 };
 
