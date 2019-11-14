17165ce05c8aad769290839108e2b649d5c8af91
schmitt.simon2 <schmitt.simon2@348A07>
2019-11-14 15:15:50 +0100

    Gegner1

--- Beispielprojekt/Spiel.cpp
+++ Beispielprojekt/Spiel.cpp
@@ -18,14 +18,14 @@ public:
 	int y_pos;
 	int leben;
 	Gosu::Image bild;
+	void positioniere(int x, int y, Gosu::Image bild) {
+		bild.draw(x, y, 0.0);
+		this-> x_pos = x;
+		this-> y_pos = y;
+	}
 
-	Figur() : bild()
-	
+	Figur(std::string dateiname) : bild(dateiname)
 	{}
-};
-
-class Spieler : public Figur {
-
 };
 
 
@@ -38,12 +38,13 @@ public:
 	{
 		set_caption("Github Test"); // Name lautet nicht Qwertz
 	}
+	Figur Gegner1("GegnerTyp1.png");
 
 	// wird bis zu 60x pro Sekunde aufgerufen.
 	// Wenn die Grafikkarte oder der Prozessor nicht mehr hinterherkommen,
 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
-	void draw() override
-	{
+	void draw() override{
+		Gegner1.postioniere(100,100);
 	}
 
 	// Wird 60x pro Sekunde aufgerufen
