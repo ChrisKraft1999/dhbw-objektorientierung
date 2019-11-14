586dee6bcc2fca4ced1a3f7dc32a6ffefb6e13e5
schmitt.simon2 <schmitt.simon2@348A07>
2019-11-14 15:26:57 +0100

    Gegner 1 fertig

--- Beispielprojekt/Spiel.cpp
+++ Beispielprojekt/Spiel.cpp
@@ -18,12 +18,11 @@ public:
 	int y_pos;
 	int leben;
 	Gosu::Image bild;
-	void positioniere(int x, int y, Gosu::Image bild) {
+	void positioniere(int x, int y) {
 		bild.draw(x, y, 0.0);
 		this-> x_pos = x;
 		this-> y_pos = y;
 	}
-
 	Figur(std::string dateiname) : bild(dateiname)
 	{}
 };
@@ -32,19 +31,18 @@ public:
 class GameWindow : public Gosu::Window
 {
 public:
-	Gosu::Image bild;
 	GameWindow()
-		: Window(800, 600)
+		: Window(800, 600), Gegner1("GegnerTyp1.png")
 	{
 		set_caption("Github Test"); // Name lautet nicht Qwertz
 	}
-	Figur Gegner1("GegnerTyp1.png");
+	Figur Gegner1;
 
 	// wird bis zu 60x pro Sekunde aufgerufen.
 	// Wenn die Grafikkarte oder der Prozessor nicht mehr hinterherkommen,
 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
 	void draw() override{
-		Gegner1.postioniere(100,100);
+		Gegner1.positioniere(100,100);
 	}
 
 	// Wird 60x pro Sekunde aufgerufen
